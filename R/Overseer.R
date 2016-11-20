db0f5695984bb4055cdc3dbffc81025f1798f5db
Devin Pastoor <devin.pastoor@gmail.com>
2016-11-19 21:56:11 -0500

    add models from a directory

--- R/Overseer.R
+++ R/Overseer.R
@@ -74,6 +74,17 @@ Overseer <- R6Class("Overseer",
                                                                              private$dir,
                                                                              soloc = private$cache_location)
                             },
+                            add_model_directory = function(.dir = ".", pattern = "*.cpp") {
+                                cpp_files <- dir(.dir, pattern = pattern)
+                                project_dir <- normalizePath(file.path(private$dir, .dir))
+                                for (i in seq_along(files)) {
+                                    .file <- cpp_files[i]
+                                    if (verbose) {
+                                        message('adding model ', .file)
+                                    }
+                                    private$models[[model_name]] <<- mread_cache(.file, project_dir, soloc = private$cache_location)
+                                }
+                            },
                             use = function(model_name) {
                                 if (is.numeric(model_name)) {
                                     warning("be careful referencing models by index as changes could result in suble bugs,
