99a00eb07fffe98c4b81f6bf5904758de6370b1b
Devin Pastoor <devin.pastoor@gmail.com>
2017-01-05 21:21:50 -0500

    more combinations

--- test_creation.R
+++ test_creation.R
@@ -15,14 +15,22 @@ $CAPTURE DV
 # save inlined model
 models$add_model(mod_simple)
 
-# pull in model saved as one_cmt_f.cpp
-models$add_model_file("one_cmt_f")
+# pull this model in if it doesn't exist on this computer
+models$add_remote_model("https://raw.githubusercontent.com/dpastoor/example_overseer/master/models/one_cmt_f.cpp")
+
+# pull in model saved as one_cmt_f.cpp and save with a different name
+models$add_model_file("one_cmt_f", "othername")
+
+# models$add_model_directory("tmpdir")
 
 models$available()
 
-models$add_remote_model("https://raw.githubusercontent.com/dpastoor/example_overseer/master/models/one_cmt_f.cpp")
 models$add_remote_model("https://raw.githubusercontent.com/dpastoor/example_overseer/master/models/multiple_models/one_cmt_f2.cpp")
+
 # access models at any time with use
+library(dplyr)
 models$use("one_cmt_f") %>%
     ev(amt = 100, cmt = 1) %>% mrgsim %>% plot
 
+models$use("othername") %>%
+    ev(amt = 100, cmt = 1) %>% mrgsim %>% plot
