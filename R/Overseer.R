939136f857a04b5aec76d1cfa58bf0076ff61ac4
Devin Pastoor <devin.pastoor@gmail.com>
2017-01-06 09:08:59 -0500

    filepath relative to model_dir so need to append modeldir

--- R/Overseer.R
+++ R/Overseer.R
@@ -82,7 +82,8 @@ Overseer <- R6Class("Overseer",
                                 # for example:
                                 # Theoph --> Theoph.cpp
                                 # Theoph.cpp --> Theoph.cpp
-                                model_path <- normalizePath(paste0(strip_ext(.filepath), ".cpp"))
+                                model_path <- normalizePath(paste0(file.path(private$dir,
+                                                                             strip_ext(.filepath)), ".cpp"))
                                 if(!file.exists(model_path)) {
                                     stop(paste0("model file not detected at: ", .filepath))
                                 }
